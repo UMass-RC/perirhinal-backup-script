ecc7f64640808f9c32311f1841ba6de585c4ed35
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-03-31 18:27:48 -0400

    working progress

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [-f "config.sh"]; then
+if [ -f "config.sh" ]; then
     source config.sh  # config file should be in the same folder
 else
     echo "Unable to find config.sh file"
@@ -10,8 +10,19 @@ fi
 timestamp=$(date '+%Y-%m-%d.%H-%M-%S')  # current timestamp
 last_timestamp=$(<lastBackup)  # read last timestamp
 
-echo "Starting sync..."
-rsync -aPh --delete -e "ssh -i $vm_script_dir/$backup_destination_key" --link-dest=$backup_destination_user@$backup_destination_host:$backup_destination_folder/$last_timestamp $vm_sync_dir $backup_destination_user@$backup_destination_host:$backup_destination_folder/$timestamp
+ssh_line="ssh -i $vm_script_dir/$backup_destination_key"
+ssh_host="$backup_destination_user@$backup_destination_host"
+
+echo "Setting up remote environment..."
+total_backup_size=$($ssh_line $ssh_host "mkdir -p $backup_destination_mount/$backup_destination_folder")
+echo "...Done"
+
+echo "Starting sync for timestamp $timestamp..."
+rsync -aPh --delete -e "ssh -i $vm_script_dir/$backup_destination_key" --link-dest=$backup_destination_folder/$backup_destination_folder/$last_timestamp $vm_sync_dir/* $backup_destination_user@$backup_destination_host:$backup_destination_mount/$backup_destination_folder/$timestamp
 echo "...Done backing up"
+
+
+total_backup_size=$($ssh_line $ssh_host "df -h --max-tree-depth=1 $backup_destination_folder/$backup_destination_folder")
+echo "Your backup is using $total_backup_size of space"
 
 echo $timestamp > lastBackup
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [-f "config.sh"]; then
+if [ -f "config.sh" ]; then
     source config.sh  # config file should be in the same folder
 else
     echo "Unable to find config.sh file"
@@ -8,13 +8,18 @@ else
 fi
 
 echo "Creating sync directories on the client..."
-mkdir -p "${vm_sync_dirs[@]}"  # create sync directories if they don't exist
+mkdir -p $vm_sync_dir  # create sync directories if they don't exist
 echo -e "...Done\n"
+
+if grep -q $vm_script_dir crons; then
+    sed "/$vm_script_dir/d" crons
+fi
 
 echo "Adding cron entry for backup"
 (crontab -l 2>/dev/null; echo "$vm_sync_frequency $vm_script_dir/backup.sh") | crontab -  # add to crontab
 echo -e "...Done\n"
 
 echo "Adding binary backup-sync"
-echo -e "#!/bin/bash\n$DIR/backup.sh" > /usr/local/bin/backup-sync  # add backup-sync binary
+echo -e "#!/bin/bash\n\n$vm_script_dir/backup.sh" > ~/.local/bin/backup-sync  # add backup-sync binary
+chmod +x ~/.local/bin/backup-sync
 echo -e "...Done\n"
